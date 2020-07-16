1112ada5853180f3a26ebaa92f0ee8196beca2d6
Immo Landwerth <immol@microsoft.com>
2020-07-16 08:15:58 -0700

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    [Obsolete(DiagnosticId = "IMMO0001")]
+    public class Class1
+    {
+    }
+
+    [Obsolete("Nope")]
+    public class Class2
+    {
+    }
+
+    [Obsolete]
+    public class Class3
+    {
+    }
+}
---
+++ ConsoleApp39/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using ClassLibrary1;
+
+namespace ConsoleApp39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new Class1();
+            var c2 = new Class2();
+            var c3 = new Class3();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
